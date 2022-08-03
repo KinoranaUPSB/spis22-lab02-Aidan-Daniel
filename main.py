dbe22423e7d6d0021c93e5c77923059939632c5e
aidanmai <70081715+KinoranaUPSB@users.noreply.github.com>
2022-08-03 20:27:10 +0000

    added sumDigits function

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 # The goal of this program is to practice Python constructs
 
+def getNumber():
+
+   symbols = input("Enter a digit: ")
+
+   number = int(symbols)
+
+   return number
+
+
 def sumTwo(a,b):
 
    c = a + b
@@ -7,7 +16,11 @@ def sumTwo(a,b):
    return c
 
 
+def sumDigits(x):
+
+  return sum([int(i) for i in str(x)])
+
 
 x = sumTwo(3,5)
 
-print(x)
+print(x) 
